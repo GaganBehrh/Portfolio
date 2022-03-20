2243d09428cb53326d51dc4717b2537515d8f375
Gagan Behrh <gaganpreet.behrh3@gmail.com>
2022-03-20 07:44:47 +0100

    alert function added to submit button

---
+++ src/script.js
@@ -0,0 +1,6 @@
+function showmessage() {
+  alert("We will get back soon to you!!!Till then have a good time!!... 😀");
+}
+
+let submitbutton = document.getElementById("contactme-submit");
+submitbutton.addEventListener("click", showmessage);
